c90c1f5b0d111b54aaf50280af95a9cd92ecf15c
NING-XUHUI <892897986@qq.com>
2020-04-14 11:09:28 +0800

    java

--- src/class/AnimalTest.java
+++ src/class/AnimalTest.java
@@ -1,13 +1,18 @@
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * @author ningxuhui
+ */
 public class AnimalTest {
-    public static void haveLunch(Animal a) {
+    public static void haveLunch(@NotNull Animal a) {
         a.eat();
     }
 
     public static void main(String[] args) {
         Animal[] as = new Animal[4];
-        as[0] = new Cat();
+        as[0] = (Animal) new Cat();
         as[1] = new Dog();
-        as[2] = new Cat();
+        as[2] = (Animal) new Cat();
         as[3] = new Dog();
 
         for (int i = 0; i < as.length; i++) {
@@ -16,7 +21,7 @@ public class AnimalTest {
         for (int i = 0; i < as.length; i++) {
             haveLunch(as[i]);
         }
-        haveLunch(new Cat());
+        haveLunch((Animal) new Cat());
         haveLunch(new Dog());
         haveLunch(
                 new Animal() {
