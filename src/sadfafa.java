0bd832e99c1e519117923c2840058ff8898efe8f
NING-XUHUI <892897986@qq.com>
2020-10-02 01:12:45 +0800

    java

--- src/sadfafa.java
+++ src/sadfafa.java
@@ -1,9 +1,24 @@
 public class sadfafa {
-    public static void main(String[] args) {
-        int a = 1;
-        if(){
-            System.out.println("adsadas");
-            
+        static public int fib(int N) {
+            if (N <= 1) {
+                return N;
+            }
+            if (N == 2) {
+                return 1;
+            }
+
+            int current = 0;
+            int prev1 = 1;
+            int prev2 = 1;
+
+            for (int i = 3; i <= N; i++) {
+                current = prev1 + prev2;
+                prev2 = prev1;
+                prev1 = current;
+            }
+            return current;
         }
+    public static void main(String[] args) {
+        System.out.println(fib(30));
     }
 }
